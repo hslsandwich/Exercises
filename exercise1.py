3d7c79f7c22da4146ae3de43e3735b95b217239c
hslsandwich <36612272+hslsandwich@users.noreply.github.com>
2018-02-19 13:10:10 +0800

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+print "Hello world"
+age=input("ilan pera mo? ")
+print "meron kang", (age), "pesos"
+wii=input("ang isang wii ay magkano? ")
+print "ang isang wii ay may halagang", (wii), "pesos"
+print "pwede ka magkaroon ng", int((age)/(wii)), "na wii"
+natira=age%wii
+print "at pwede ka rin bumili ng wii pa pag meron nakaipon ka pa ulit ng", (wii)-(natira), "pesos"
---
+++ exercise2.py
@@ -0,0 +1,7 @@
+from math import *
+
+sum=1
+n=input("input value? ")
+for i in range(1,n+1):
+    sum=sum*(i)
+print "the value of", n, "factorial is ", sum
